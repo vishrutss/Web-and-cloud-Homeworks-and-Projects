b8669ead3d4fc59d838dbe806b959fb60827e2f2
Vishrut Sharma <vishrut@pdx.edu>
2023-04-21 20:11:05 -0700

    Changed listening port to 5000

--- hw2/app.py
+++ hw2/app.py
@@ -17,4 +17,4 @@ app.add_url_rule('/submit',
                  methods=['GET', 'POST'])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
